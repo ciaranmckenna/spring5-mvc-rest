96b89f8869ec422847e30193028cd5001583343a
ciaranmckenna <hello@ciaranmckenna.com>
2023-10-30 16:29:21 +0000

    Error filled file fixed by hook

--- src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -3,34 +3,31 @@ package guru.springfamework.services;
 import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.repositories.CategoryRepository;
-import org.springframework.stereotype.Service;
-
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final CategoryMapper categoryMapper;
-    private final CategoryRepository categoryRepository;
+  private final CategoryMapper categoryMapper;
+  private final CategoryRepository categoryRepository;
 
-    public CategoryServiceImpl(final CategoryMapper categoryMapper, final CategoryRepository categoryRepository) {
-        this.categoryMapper = categoryMapper;
-        this.categoryRepository = categoryRepository;
-    }
+  public CategoryServiceImpl(
+      final CategoryMapper categoryMapper, final CategoryRepository categoryRepository) {
+    this.categoryMapper = categoryMapper;
+    this.categoryRepository = categoryRepository;
+  }
 
-    @Override
-    public List<CategoryDTO> getAllCategories() {
-        return categoryRepository.findAll()
-                .stream()
-                .map(categoryMapper::categoryToCategoryDTO)
-                .collect(Collectors.toList());
-    }
+  @Override
+  public List<CategoryDTO> getAllCategories() {
+    return categoryRepository.findAll().stream()
+        .map(categoryMapper::categoryToCategoryDTO)
+        .collect(Collectors.toList());
+  }
 
-    @Override
-    public CategoryDTO getCategoryByName(String name) {
-        Timestamp timestampTemp = new Timestamp(1L);
-        return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
-    }
+  @Override
+  public CategoryDTO getCategoryByName(String name) {
+    return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
+  }
 }
